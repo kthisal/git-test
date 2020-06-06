917bd1d9e7de063a51e387948f9dc978fe790c64
Kavindu <kavindu.20191098@iit.ac.lk>
2020-06-07 01:58:43 +0530

    third Change

--- new.py
+++ new.py
@@ -2,3 +2,5 @@ Python 3.7.1 (v3.7.1:260ec2c36a, Oct 20 2018, 14:05:16) [MSC v.1915 32 bit (Inte
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> 
 print("Welcome to GIT")
+Welcome to GIT
+>>> 
